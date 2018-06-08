00c13f59cbf4d6b922413e4d8f2e2baf4127f547
masmedia <mustafa.sari.media@gmail.com>
2018-06-08 09:10:56 +0300

    Added courses component and registered it into app module

---
+++ src/app/courses.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// Decorator to make Angular understand this component (courses)
+@Component({
+    selector: 'courses',            // CSS Selector 
+    template: '<h1>Courses</h1>'    // HTML Markup
+})
+
+export class CoursesComponent {
+ 
+}
