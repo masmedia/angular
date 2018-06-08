2a9bc9f83a2d61fca8416f3fc1a2b4ad124e0511
masmedia <mustafa.sari.media@gmail.com>
2018-06-08 09:35:18 +0300

    Modified template to display a string object.

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,9 +3,14 @@ import { Component } from '@angular/core';
 // Decorator to make Angular understand this component (courses)
 @Component({
     selector: 'courses',            // CSS Selector 
-    template: '<h1>Courses</h1>'    // HTML Markup
+    template: '<h2>{{ getTitle() }}</h2>'    // HTML Markup
 })
 
 export class CoursesComponent {
+    title = "List of courses";
+
+    getTitle() {
+        return this.title;
+    }
  
 }
